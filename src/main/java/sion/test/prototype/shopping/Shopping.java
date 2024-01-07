5460aec9ea80eb456a531a5dd16117008fb722c7
Sion <staythismoment.sion@gmail.com>
2024-01-07 23:32:07 +0900

    구매 dto 필드 변경

--- src/main/java/sion/test/prototype/shopping/Shopping.java
+++ src/main/java/sion/test/prototype/shopping/Shopping.java
@@ -5,11 +5,15 @@ import lombok.Setter;
 import sion.test.prototype.cancel.Cancel;
 import sion.test.prototype.order.Order;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class Shopping {
     private String type; // TODO Enum - ORDER, CANCEL
+    private Long userId;
     private String productName;
-    private Order order;
-    private Cancel cancel;
+    private Integer totalPrice;
+    private Integer quantity;
+    private LocalDateTime createdAt; // 구매 혹은 취소일자
 }
